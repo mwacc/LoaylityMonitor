99d100790b8d70193cd7b77a212c146d22605bf9
Kostiantyn_Kudriavts <kostiantyn_kudriavtsev@epam.com>
2013-05-29 12:21:32 +0300

    Fix bug with stoping flow after first execution

--- batch-admin/src/main/java/loaylitymonitor/quartz/RunBaseAggregationJob.java
+++ batch-admin/src/main/java/loaylitymonitor/quartz/RunBaseAggregationJob.java
@@ -5,9 +5,20 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.converter.DefaultJobParametersConverter;
+import org.springframework.batch.core.converter.JobParametersConverter;
 import org.springframework.batch.core.launch.support.CommandLineJobRunner;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.StringUtils;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class RunBaseAggregationJob implements Job {
     private static Logger logger = LoggerFactory.getLogger(RunBaseAggregationJob.class);
@@ -19,10 +30,11 @@ public class RunBaseAggregationJob implements Job {
         logger.info("Cron start executing ");
         try {
             // TODO: run spring batch job
-            CommandLineJobRunner.main(new String[]{"classpath:/spring/spring-context.xml", "baseAggJob"});
+            CommandLineJobRunner.main(new String[]{"classpath:/spring/spring-context.xml", "baseAggJob", "startAt="+String.valueOf(System.currentTimeMillis()) });
         } catch (Exception e) {
             logger.error("Can't run spring batch 'baseAggJob' job" + e);
         }
 
     }
+
 }
